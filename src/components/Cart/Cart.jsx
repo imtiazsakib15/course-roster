b98429c46189690f132566b20c77a4a67516e6cf
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-09-14 23:58:50 +0600

    static cart design added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,20 @@
-
 const Cart = () => {
-    return (
-        <div className="w-1/4">
-            Cart
-        </div>
-    );
+  return (
+    <div className="w-1/4 h-max bg-white p-6 rounded-xl">
+      <h4 className="text-lg font-bold text-blue-500 leading-relaxed pb-4 border-b-2">
+        Credit Hour Remaining 7 hr
+      </h4>
+      <div className="border-b-2 py-6">
+        <h3 className="text-xl font-bold">Course Name</h3>
+      </div>
+      <div className="border-b-2 py-6">
+        <h5 className="font-medium">Total Credit Hour : 13</h5>
+      </div>
+      <div className="pt-6">
+        <h5 className="font-semibold">Total Price : 48000 USD</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
